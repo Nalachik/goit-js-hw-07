2907ef1081cdaa5726a7c2f707f1e98313bd2626
Maksym Kurylo <Nalachik@ukr.net>
2022-02-13 22:08:50 +0200

    add script basicLigthBox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+ 
+const galleryRef = document.querySelector('.gallary');
+
+const markup = galleryRef.map((item) => 
+`<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+     <img
+      class="gallery__image"
+      src='${item.preview}'
+      data-source='${item.original}'
+      alt='${item.description}'
+    />
+  </a>
+</div>`).join('');
+
+galleryRef.innerHTML = markup
+
