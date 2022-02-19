9c29289247fc1719b6355f70bcdf8da0569e263c
Maksym Kurylo <Nalachik@ukr.net>
2022-02-19 20:00:31 +0200

    update task 2
    add SimpleLightbox options

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+galleryRef = document.querySelector('.gallery');
+
+const markup = galleryItems.map((item) =>
+ `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src='${item.preview}'
+      data-source='${item.original}'
+      alt='${item.description}'
+    />
+  </a>
+</div>`).join('');
+
+galleryRef.innerHTML = markup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
